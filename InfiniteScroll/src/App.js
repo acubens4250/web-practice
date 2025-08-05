06f21baa2b425b5d057b4a07179af118aba8f1b1
acubens4250 <acubens425@naver.com>
2025-08-05 10:40:18 +0900

    api 사진 호출 불가

---
+++ InfiniteScroll/src/App.js
@@ -0,0 +1,34 @@
+import PhotoList from './PhotoList.js'
+import { request } from './api.js'
+
+export default function App({ $target }) {
+    const $h1 = document.createElement('h1')
+    $h1.innerHTML - 'Cat Photos!'
+    $h1.style.textAlign = 'center'
+    $target.appendChild($h1)
+
+    this.state = {
+        limit: 5,
+        start: 0,
+        photos: [],
+    }
+    const photoListComponent = new PhotoList({
+        $target,
+        initialState: this.state.photos
+    })
+
+    this.setState = (nextState) => {
+        this.state = nextState
+        photoListComponent.setState(nextState.photos)
+    }
+
+    const fetchPhotos = async() => {
+        const photos = await request(`/cat-photos?_limit=5&start=0`)
+        this.setState({
+            ...this.state,
+            photos
+        })
+    }
+
+    fetchPhotos()
+}
