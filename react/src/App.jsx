db01e84b90b9c9e5bf568e7008ea17594b1c0f3c
acubens4250 <acubens4250@naver.com>
2025-09-01 20:01:48 +0900

    app.jsx 변경, 서버 안 켜짐

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  return <>Hello World</>;
+  const [pocketmons, setPocketmons] = useState([]);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then((res) => res.json())
+      .then((res) => setPocketmons(res.results));
+  });
 
   return (
     <>
-      
+      <h1>포켓몬 도감</h1>
+      <ul>
+        {pocketmons.map((pocketmon, index) => (
+          <li key={index}>{pocketmon.name}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
